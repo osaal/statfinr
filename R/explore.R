590ebc85203ffa6b1f022902c880e193a5350fc7
Oliver Saal <29450366+osaal@users.noreply.github.com>
2024-03-15 22:34:03 +0200

    Add pattern matching error handling to explore, write explore documentation

--- R/explore.R
+++ R/explore.R
@@ -9,14 +9,24 @@
 #' specific database ID with the `db` argument, it returns the sub-databases
 #' for that database.
 #'
-#' @param db The id of a specific database
+#' @param db The id of a specific database (optional)
 #'
 #' @returns A data frame containing the `id` and `name` of the databases
 #' @export
 explore <- function(db = NULL) {
+  # TODO: Write unit tests
   address <- "https://statfin.stat.fi/PXWeb/api/v1/fi/StatFin/"
 
   if (!is.null(db)) {
+    # Throw error if db contains anything but letters and numbers
+    if (stringr::str_detect(db, pattern = "[\\s[:punct:]]")) {
+      cli::cli_abort(c(
+        "{.var db} must only contain alphanumeric characters",
+        "i" = "You entered {db}",
+        "i" = "Enter the id of a valid database to retrieve its sub-databases"
+      ))
+    }
+
     address <- paste(
       address,
       db,
